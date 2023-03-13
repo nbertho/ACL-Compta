bc05ac0c0c744ef6a6155406a454b272ecf082da
Nicolas Bertho <n.bertho.web@gmail.com>
2023-03-13 20:37:25 +0100

    Added close option on popup

--- resources/js/components/assets/ActionMsg.vue
+++ resources/js/components/assets/ActionMsg.vue
@@ -1,8 +1,9 @@
 <template>
   <div 
-    :class="'border border-black mb-4 rounded shadow-sm p-4 w-3/12 right-6 absolute z-50 ' + bgColorClass"
+    :class="'border border-black mb-4 rounded shadow-sm px-4 pb-4 pt-2 w-3/12 right-6 absolute z-50 ' + bgColorClass"
     v-if="msg !== ''"
   >
+    <div class="close cursor-pointer" @click="emitCloseEvent">&nbsp;</div>
     <p>{{ msg }}</p>
     <ul v-if="errors" class="pl-4">
       <li v-for="error in errors" :key="error" class="list-disc">{{ error }}</li>
@@ -28,12 +29,30 @@ export default({
       default: 'bg-green-400'
     }
   },
+  methods: {
+    emitCloseEvent() {
+      this.$emit('resetMsg');
+    }
+  },
   updated() {
     if (this.msg !== '') {
       setTimeout(() => {
-        this.$emit('resetMsg');
-      }, 5000);
+        this.emitCloseEvent();
+      }, 3000);
     }
   }
 });
 </script>
+
+<style scoped>
+.close {
+  width: 25px;
+  height: 25px;
+  margin-right: 0;
+  margin-left: auto;
+}
+.close:after{
+  content: "\00d7";
+  font-size: 25px;
+}
+</style>
